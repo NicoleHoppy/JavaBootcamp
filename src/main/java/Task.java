2e3a7261335f2b60d791629c6894f4986c16b442
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-07-05 16:29:11 +0200

    Initial commit in project

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,26 @@
 public class Task {
 
     public static void main(String[] args) {
+        byte varByte = 127;
+        short varShort = 32767;
+        int varInt = 21;
+        long varLong = 526L;
+        float varFloat = 2.385774F;
+        double varDouble = 2.34;
+        boolean varBoolean = true;
+        char varChar = 'A';
+
         System.out.println("This is my first Java task!");
+
+        System.out.println("varByte: " + varByte + ", varShort: " + varShort + ", varInt: " + varInt + ", varLong: " + varLong);
+
+        // System.out.println("varByte: " + varByte);
+        // System.out.println("varShort: " + varShort);
+        // System.out.println("varInt: " + varInt);
+        // System.out.println("varLong: " + varLong);
+
+        System.out.println("varFloat: " + varFloat + ", varDouble: " + varDouble + ", varBoolean: " + varBoolean + ", varChar: " + varChar);
+
+
     }
 }
