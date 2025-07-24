5875e23f38ece43206db104bb4388d7228bac8dc
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-07-24 14:41:28 +0200

    feat: create Ex06HowManyTimes class to count occurrences of number in array

---
+++ src/main/java/pl/javaexercises/week01/section02/Ex06HowManyTimes.java
@@ -0,0 +1,27 @@
+package pl.javaexercises.week01.section02;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import pl.javaexercises.week01.Task;
+
+//    6. Write a program that counts how many times a given number appears in the array.
+
+public class Ex06HowManyTimes implements Task<String> {
+
+    int[] table;
+
+    public Ex06HowManyTimes(int[] table) {
+        this.table = table;
+    }
+
+    public String run() {
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int t : table) {
+            counts.put(t, counts.getOrDefault(t, 0) + 1);
+        }
+
+        return "For the following table: " + Arrays.toString(table) + " numbers appear this many times: " + counts;
+    }
+}
--- src/main/java/pl/javaexercises/week01/section02/MainWeek01Section02.java
+++ src/main/java/pl/javaexercises/week01/section02/MainWeek01Section02.java
@@ -10,8 +10,9 @@ public class MainWeek01Section02 {
             new Ex01TableOfFiveNumbers(new int[] {1, 2, 3, 4, 5}),
             new Ex02CountNumbersAboveTen(new int[] {1, 2, 13, 16, 49}),
             new Ex03MaximumInTable(new int[] {1, 25, 3, 4, 5}),
-            new Ex04SumOfTable(new int[]{1, 2, 3, 4, 5}),
-                new Ex05ReversedTable(new int[]{1, 2, 3, 4, 5}));
+            new Ex04SumOfTable(new int[] {1, 2, 3, 4, 5}),
+            new Ex05ReversedTable(new int[] {1, 2, 3, 4, 5}),
+            new Ex06HowManyTimes(new int[]{1, 2, 3, 4, 3, 12, 2, 2, 2}));
 
         tasks.forEach(task -> System.out.println(task.run()));
     }
