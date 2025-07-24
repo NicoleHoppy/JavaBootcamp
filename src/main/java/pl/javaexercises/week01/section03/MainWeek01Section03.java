5ef83d5eb82c368231fc18311487506f6680b7a9
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-07-24 15:30:58 +0200

    feat: create Ex04AboveEighteen to print people older than 18 from map

--- src/main/java/pl/javaexercises/week01/section03/MainWeek01Section03.java
+++ src/main/java/pl/javaexercises/week01/section03/MainWeek01Section03.java
@@ -9,7 +9,8 @@ public class MainWeek01Section03 {
         List<Task<String>> tasks = List.of(
                 new Ex01ListOfNames(List.of("Kate", "Peter", "Anna", "Tom")),
                 new Ex02IsContaining(List.of("Kate", "Peter", "Anna")),
-                new Ex03MapOfNamesAges(Map.of("Anne", 20, "Kate", 25))
+                new Ex03MapOfNamesAges(Map.of("Anne", 20, "Kate", 25)),
+                new Ex04AboveEighteen(Map.of("Tom", 17, "Ann", 20, "Kate", 30))
         );
 
         tasks.forEach(task -> System.out.println(task.run()));
