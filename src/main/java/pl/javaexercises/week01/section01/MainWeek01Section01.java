f0ca4f2e6a17bd517a29ed5e2a4773cc18775f18
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-07-24 10:36:16 +0200

    feat: create Ex01StandardLoop class to print numbers from 1 to 10

---
+++ src/main/java/pl/javaexercises/week01/section01/MainWeek01Section01.java
@@ -0,0 +1,14 @@
+package pl.javaexercises.week01.section01;
+
+import java.util.List;
+import pl.javaexercises.week01.Task;
+
+public class MainWeek01Section01 {
+    public static void main(String[] args) {
+
+        List<Task<String>> tasks = List.of(
+                new Ex01StandardLoop());
+
+        tasks.forEach(task -> System.out.println(task.run()));
+    }
+}
