bf4fd2e486623377da7ec923e163e0c56fb7ddcb
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-07-24 17:00:02 +0200

    feat: create Ex07TodoTaskPrinter to map status → list of tasks and print 'TODO'

---
+++ src/main/java/pl/javaexercises/week01/section03/Ex07TodoTaskPrinter.java
@@ -0,0 +1,29 @@
+package pl.javaexercises.week01.section03;
+
+import pl.javaexercises.week01.Task;
+
+import java.util.Map;
+
+//7. Add tasks to a map of status → list of tasks and print all tasks with theTODO status.
+
+public class Ex07TodoTaskPrinter implements Task<String> {
+
+    Map<String, String> statusMap;
+
+    public Ex07TodoTaskPrinter(Map<String, String> statusMap) {
+        this.statusMap = statusMap;
+    }
+
+    public String run() {
+
+        StringBuilder onlyToDo = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : statusMap.entrySet()) {
+            if (entry.getValue().equalsIgnoreCase("TODO")) {
+                onlyToDo.append("Name: ").append(entry.getKey()).append(", Status: ").append(entry.getValue()).append("\n");
+            }
+        }
+
+        return "The tasks with the TODO status: " + "\n" + onlyToDo + "\n";
+    }
+}
--- src/main/java/pl/javaexercises/week01/section03/MainWeek01Section03.java
+++ src/main/java/pl/javaexercises/week01/section03/MainWeek01Section03.java
@@ -13,7 +13,8 @@ public class MainWeek01Section03 {
             new Ex03MapOfNamesAges(Map.of("Anne", 20, "Kate", 25)),
             new Ex04AboveEighteen(Map.of("Tom", 17, "Ann", 20, "Kate", 30)),
             new Ex05NameOccurrenceCounter(List.of("Tom", "Ann", "Emma", "Joe", "Emma", "Kate", "Josh", "Joe")),
-        new Ex06TaskAndDone(Map.of("Cooking", "Done", "Cleaning", "done", "Shopping", "not done")));
+        new Ex06TaskAndDone(Map.of("Cooking", "Done", "Cleaning", "done", "Shopping", "todo")),
+        new Ex07TodoTaskPrinter(Map.of("Cooking", "Done", "Cleaning", "done", "Shopping", "todo")));
 
         tasks.forEach(task -> System.out.println(task.run()));
     }
