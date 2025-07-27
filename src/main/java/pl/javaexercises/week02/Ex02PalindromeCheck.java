845b83d4015b14f8bd8c1012cb37600c0fe257dc
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-07-27 18:11:15 +0200

    feat: create Ex02PalindromeCheck class to check if the word is a palindrome

---
+++ src/main/java/pl/javaexercises/week02/Ex02PalindromeCheck.java
@@ -0,0 +1,14 @@
+package pl.javaexercises.week02;
+
+// 2. Write a method that counts how many vowels (a, e, i, o, u, y) are present in the given String, regardless of case.
+
+public class Ex02PalindromeCheck implements TaskWeek02<Boolean>{
+
+    @Override
+    public Boolean run(String input) {
+        String lowerCaseNoSpace = input.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(lowerCaseNoSpace).reverse().toString();
+        return (lowerCaseNoSpace.equals(reversed));
+    }
+}
+
--- src/main/java/pl/javaexercises/week02/MainWeek02.java
+++ src/main/java/pl/javaexercises/week02/MainWeek02.java
@@ -7,16 +7,17 @@ public class MainWeek02 {
 
     public static void main(String[] args) {
 
-    Map<TaskWeek02<String>, List<String>> MAPTASKINPUT =
+    Map<TaskWeek02, List<String>> MAPTASKINPUT =
         Map.of(
-            new Ex01CharReplacer(), List.of("nobody@mail.com ###", "@somebody @nobody @anybody")
+            new Ex01CharReplacer(), List.of("nobody@mail.com ###", "@somebody @nobody @anybody"),
+                new Ex02PalindromeCheck(), List.of("Kobyła ma mały bok", "cat")
             );
 
-        for (Map.Entry<TaskWeek02<String>, List<String>> entry : MAPTASKINPUT.entrySet()){
-            TaskWeek02<String> task = entry.getKey();
+        for (Map.Entry<TaskWeek02, List<String>> entry : MAPTASKINPUT.entrySet()){
+            TaskWeek02 task = entry.getKey();
             System.out.printf("Class name: %s \n", entry.getKey().getClass().getSimpleName());
             for (String input : entry.getValue()){
-                String result = task.run(input);
+                String result = task.run(input).toString();
                 System.out.printf("Input: %s, Result: %s\n", input, result);
             }
         }
