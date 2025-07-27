f6d453f7fe87e72710287f9d7d56ceda1534fbc8
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-07-27 17:45:14 +0200

    docs: add comment

--- src/main/java/pl/javaexercises/week02/Ex01CharReplacer.java
+++ src/main/java/pl/javaexercises/week02/Ex01CharReplacer.java
@@ -1,5 +1,7 @@
 package pl.javaexercises.week02;
 
+// 1. Write a method that receives a String and replaces all occurrences of @ with (at) and # with (hash). The method returns a new, transformed string.
+
 public class Ex01CharReplacer implements TaskWeek02<String> {
 
     @Override
