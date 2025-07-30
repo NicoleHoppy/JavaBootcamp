95200eabf19e7479240ac15664309c0485a3f63a
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-07-30 12:50:32 +0200

    fix: specify generic type for TaskWeek02 to eliminate raw type warning

--- src/main/java/pl/javaexercises/week02/MainWeek02.java
+++ src/main/java/pl/javaexercises/week02/MainWeek02.java
@@ -8,9 +8,9 @@ public class MainWeek02 {
 
   public static void main(String[] args) {
 
-    Map<TaskWeek02, List<String>> MAP = new LinkedHashMap<>();
+    Map<TaskWeek02<String>, List<String>> MAP = new LinkedHashMap<>();
     MAP.put(new Ex01CharReplacer(), List.of("nobody@mail.com ###", "@somebody @nobody @anybody"));
-    MAP.put(new Ex02PalindromeCheck(), List.of("Kobyła ma mały bok", "cat"));
+    MAP.put(new Ex02PalindromeCheck(), List.of("Madam", "cat"));
     MAP.put(new Ex03VowelCounter(), List.of("I have a cat", "Hello world"));
     MAP.put(new Ex04InitialsGenerator(), List.of("Tom Smith", "Kate Jackson"));
     MAP.put(new Ex05CaseConverter(), List.of("programming", "HELLO"));
@@ -19,11 +19,11 @@ public class MainWeek02 {
     MAP.put(new Ex08DigitWordConverter(), List.of("I have 1 cat and 3 dogs", "My mum has 4 cats"));
     MAP.put(new Ex09TextCensor(), List.of("I am tired as fuck", "It's so damn hot"));
 
-    for (Map.Entry<TaskWeek02, List<String>> entry : MAP.entrySet()) {
-      TaskWeek02 task = entry.getKey();
-      System.out.printf("Class name: %s \n", entry.getKey().getClass().getSimpleName());
+    for (Map.Entry<TaskWeek02<String>, List<String>> entry : MAP.entrySet()) {
+      TaskWeek02<String> task = entry.getKey();
+      System.out.printf("\nClass name: %s \n", entry.getKey().getClass().getSimpleName());
       for (String input : entry.getValue()) {
-        String result = task.run(input).toString();
+        String result = task.run(input);
         System.out.printf("Input: %s, Result: %s\n", input, result);
       }
     }
