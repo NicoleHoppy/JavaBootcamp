37478593937ec4fc8373414921fe6f615b42207e
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-08-08 10:23:44 +0200

    docs: add a comment

--- src/main/java/pl/javaexercises/week03/Ex02Guitar.java
+++ src/main/java/pl/javaexercises/week03/Ex02Guitar.java
@@ -1,5 +1,7 @@
 package pl.javaexercises.week03;
 
+// 2. Create an interface Playable with the method play(). Create a class Guitar implementing Playable and displaying “Guitar is playing”.
+
 public class Ex02Guitar implements Ex02Playable {
     public void play() {
     System.out.println("Guitar is playing!");
