bee9468fc70815ae8ef93d30fafe7b63d1ee0e1a
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-08-14 11:56:49 +0200

    test: add JUnit 5 test for add() using assertEquals

---
+++ src/test/java/pl/javaexercises/week04/Ex01CalculatorTest.java
@@ -0,0 +1,11 @@
+package pl.javaexercises.week04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ex01CalculatorTest {
+
+  @Test
+  public void addTest() {assertEquals(3, Ex01Calculator.add(1, 2));}
+}
