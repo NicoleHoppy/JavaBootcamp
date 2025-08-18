d73a7de76b2418056e7d5ace0d712f9ed40de6cc
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-08-18 09:57:00 +0200

    test: add tests for reverse() - normal, empty and null cases

---
+++ src/test/java/pl/javaexercises/week04/Ex03StringReverserTest.java
@@ -0,0 +1,17 @@
+package pl.javaexercises.week04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ex03StringReverserTest {
+
+  @Test
+  void testReverseString() {assertEquals("olleH", Ex03StringReverser.reverse("Hello"));}
+
+  @Test
+  void testReverseEmpty() {assertEquals("", Ex03StringReverser.reverse(""));}
+
+  @Test
+  void testReverseNull() {assertNull(Ex03StringReverser.reverse(null), "This is null");}
+}
