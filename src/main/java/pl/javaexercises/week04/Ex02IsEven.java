549702c34d7444b090a97d04c1e72e00c64fc2c9
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-08-18 09:42:12 +0200

    feat: add isEven(int n) method to return true when n % 2 == 0

---
+++ src/main/java/pl/javaexercises/week04/Ex02IsEven.java
@@ -0,0 +1,9 @@
+package pl.javaexercises.week04;
+
+//2. Write a method isEven(int n) that returns true if the number is even. Write tests for even and odd numbers.
+
+public class Ex02IsEven {
+    public static boolean isEven (int n) {
+        return n % 2 == 0;
+    }
+}
