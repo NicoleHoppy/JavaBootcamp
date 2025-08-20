6db7780e0bbddbee38e8605ea4ff8339a949b152
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-08-20 16:51:26 +0200

    refactor: small change in the way of printing result

--- src/main/java/pl/javaexercises/week04/Ex06ConsoleLoggerService.java
+++ src/main/java/pl/javaexercises/week04/Ex06ConsoleLoggerService.java
@@ -3,6 +3,6 @@ package pl.javaexercises.week04;
 public class Ex06ConsoleLoggerService implements Ex06LoggerService{
     @Override
     public void log(String message) {
-    System.out.println("LOG: " + message);
+        MainWeek04.printResult(Ex06Calculator.class, "LOG: " + message);
     }
 }
