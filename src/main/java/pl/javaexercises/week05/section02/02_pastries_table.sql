1b4943db675a3fa641f4691184c9a57a85715947
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-09-02 20:17:15 +0200

    feat(sql): add 02_pastries_table.sql file to create and delete pastries table with name and quantity

---
+++ src/main/java/pl/javaexercises/week05/section02/02_pastries_table.sql
@@ -0,0 +1,14 @@
+-- Exercise 2:
+
+-- Create a `pastries` table.
+-- It should include 2 columns: name and quantity.
+-- Name must be a maximum of 50 characters.
+-- Inspect your table and columns in the CLI.
+-- Then delete your table!
+
+CREATE TABLE `pastries` ( 
+  `name` VARCHAR(50),
+  `quantity` INT 
+);
+
+DROP TABLE `pastries`;
