313a3cc992acaf3109f5b2d05a9c0db63376e1e0
Nikola Chmielewska <nikola.chmielewska@tutanota.com>
2026-09-16 21:14:20 +0200

    feat: add another user

--- src/main/java/pl/javaexercises/week07/section01/Ex02AddUser.java
+++ src/main/java/pl/javaexercises/week07/section01/Ex02AddUser.java
@@ -4,14 +4,21 @@ import jakarta.persistence.EntityManager;
 
 public class Ex02AddUser {
     public static void execute(EntityManager em) {
-        Ex01User user = new Ex01User();
-        user.setName("Anna");
-        user.setEmail("anna@example.com");
+
+        Ex01User user1 = new Ex01User();
+        user1.setName("Anna");
+        user1.setEmail("anna@example.com");
+
+        Ex01User user2 = new Ex01User();
+        user2.setName("Bob");
+        user2.setEmail("bob@example.com");
 
         em.getTransaction().begin();
-        em.persist(user);
+        em.persist(user1);
+        em.persist(user2);
         em.getTransaction().commit();
 
-        System.out.println("User saved: " + user.getId());
+        System.out.println("User saved: " + user1.getId());
+        System.out.println("User saved: " + user2.getId());
     }
 }
