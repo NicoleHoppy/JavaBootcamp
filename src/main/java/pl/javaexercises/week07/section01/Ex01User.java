a1c9d9e83f4f8240fed24df8ff25c8e450202050
Nikola Chmielewska <nikola.chmielewska@tutanota.com>
2026-09-16 21:13:26 +0200

    feat: add lazy one-to-many Project relationship to User

--- src/main/java/pl/javaexercises/week07/section01/Ex01User.java
+++ src/main/java/pl/javaexercises/week07/section01/Ex01User.java
@@ -2,8 +2,19 @@ package pl.javaexercises.week07.section01;
 
 import jakarta.persistence.*;
 
-// Task 1: Mark the class as an entity and add a primary key. Add name and email fields to the User entity
-// Create a User class and mark it as an entity, with the id field as the primary key. Add the name and email fields to the User entity.
+import java.util.ArrayList;
+import java.util.List;
+
+// Task 1: Mark the class as an entity and add a primary key. Add name and email fields to the User
+// entity
+// Create a User class and mark it as an entity, with the id field as the primary key. Add the name
+// and email fields to the User entity.
+
+// Task 6: Add a one-to-many (OneToMany) relationship
+// In the User class, add a list of projects (List<Project>).
+
+// Task 7: Set the LAZY fetch type for the list of projects
+// Change the default fetch type of the projects relationship to LAZY.
 
 @Entity
 public class Ex01User {
@@ -37,6 +48,9 @@ public class Ex01User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
+    private final List<Ex04Project> projects= new ArrayList<>();
 }
 
 
